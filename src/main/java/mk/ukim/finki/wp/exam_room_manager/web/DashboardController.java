d5618b2d1c581e679fa8a94289c245227e0bc370
Ognen Mladenovski <ognen.mlad@gmail.com>
2026-05-06 23:29:48 +0200

    Commit Dashboard Controller

--- src/main/java/mk/ukim/finki/wp/exam_room_manager/web/DashboardController.java
+++ src/main/java/mk/ukim/finki/wp/exam_room_manager/web/DashboardController.java
@@ -1,7 +1,27 @@
 package mk.ukim.finki.wp.exam_room_manager.web;
 
+import lombok.RequiredArgsConstructor;
+import mk.ukim.finki.wp.exam_room_manager.model.Professor;
+import mk.ukim.finki.wp.exam_room_manager.service.ProfessorService;
+import mk.ukim.finki.wp.exam_room_manager.service.SubjectService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class DashboardController {
+
+    private final ProfessorService professorService;
+    private final SubjectService subjectService;
+
+    @GetMapping("/dashboard")
+    public String dashboard(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        Professor professor = professorService.findByUsername(userDetails.getUsername());
+        model.addAttribute("professor", professor);
+        model.addAttribute("subjects", subjectService.findByProfessor(professor));
+        return "dashboard";
+    }
 }
